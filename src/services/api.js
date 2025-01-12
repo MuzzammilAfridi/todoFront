7b3041338e72b47b0bc03b9e955458904e77ae06
MuzzammilAfridi <118999159+MuzzammilAfridi@users.noreply.github.com>
2025-01-12 19:38:38 +0530

    try to deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://todoback-rho.vercel.app" });
+
+// const API = axios.create({ baseURL: "https://todoback-rho.vercel.app" });
+const API = axios.create({
+    baseURL: "https://todoback-rho.vercel.app",
+    withCredentials: true, // Add this if the backend requires credentials
+  });
 
 export const fetchTasks = () => API.get("/tasks");
 export const addTask = (task) => API.post("/tasks", task);
